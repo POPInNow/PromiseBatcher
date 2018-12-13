b213d2a429b6cb318ed632d0b5a45250adc94bab
Peter Yamanaka <peter@popinnow.com>
2018-12-13 10:39:56 -0800

    Upgrade babel to 7

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ const config = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: [ '@babel/preset-env' ]
           }
         }
       }
