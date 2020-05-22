e033500ebac6f34caefa20ca42722ca96b1e8104
Peter Yamanaka <peter@popinnow.com>
2020-05-22 16:04:22 -0700

    It looks weird, but we move functions into the constructor
    This allows us to create private scoped variables in the constructor and private methods which are only consumed by the
    exposed this. arrow functions

--- src/promise-batching-queue.js
+++ src/promise-batching-queue.js
@@ -28,171 +28,161 @@ export class PromiseBatchingQueue {
       throw Error('maxSize must be greater than 0');
     }
 
-    this.__maxSize = maxSize;
-    this.__activeQueue = [];
-    this.__waitingQueue = [];
+    const activeQueue = [];
+    let waitingQueue = [];
 
-    this.__notifyWaiting = this.__notifyWaiting.bind(this);
-    this.__firePromise = this.__firePromise.bind(this);
-    this.__queuePromise = this.__queuePromise.bind(this);
-    this.__removeItemFromArray = this.__removeItemFromArray.bind(this);
+    /**
+     * Remove the item with the given id from the active queue
+     * @private
+     *
+     * @param {string} id
+     * @return {QueueObject|null} Item removed
+     */
+    const removeItemFromArray = (id) => {
+      const queue = activeQueue;
+      const size = queue.length;
+      for (let i = 0; i < size; ++i) {
+        const item = queue[i];
+        if (id === item.getId()) {
+          queue.splice(i, 1);
+          return item;
+        }
+      }
 
-    this.clear = this.clear.bind(this);
-    this.queue = this.queue.bind(this);
-    this.getActiveSize = this.getActiveSize.bind(this);
-    this.getWaitingSize = this.getWaitingSize.bind(this);
-    this.getMaxSize = this.getMaxSize.bind(this);
-  }
+      return null;
+    };
 
-  /**
-   * Return the current size of the active queue
-   * @return {number}
-   */
-  getActiveSize () {
-    return this.__activeQueue.length;
-  }
 
-  /**
-   * Return the current size of the waiting queue
-   * @return {number}
-   */
-  getWaitingSize () {
-    return this.__waitingQueue.length;
-  }
-
-  /**
-   * Return the max size of the active queue
-   * @return {number}
-   */
-  getMaxSize () {
-    return this.__maxSize;
-  }
-
-  /**
-   * Clear the queues on demand
-   */
-  clear () {
-    // Clear the waiting queue first, any active promises are already active and so will be finished
-    // unless they are cancelled via other means.
-    this.__waitingQueue = [];
-  }
-
-  /**
-   * Remove the item with the given id from the active queue
-   * @private
-   *
-   * @param {string} id
-   * @return {QueueObject|null} Item removed
-   */
-  __removeItemFromArray (id) {
-    const queue = this.__activeQueue;
-    const size = queue.length;
-    for (let i = 0; i < size; ++i) {
-      const item = queue[i];
-      if (id === item.getId()) {
-        queue.splice(i, 1);
-        return item;
+    /**
+     * Notify any waiting members in the queue if there is space in the active queue
+     * @private
+     */
+    const notifyWaiting = () => {
+      // The waiting queue has been notified, if a promise can run, do it
+      if (activeQueue.length < maxSize) {
+        if (waitingQueue.length > 0) {
+          // Shift the first item out of the waiting queue
+          const firstWaiting = waitingQueue.shift();
+          firePromise(firstWaiting);
+        }
       }
     }
 
-    return null;
-  };
 
-  /**
-   * Notify any waiting members in the queue if there is space in the active queue
-   * @private
-   */
-  __notifyWaiting () {
-    // The waiting queue has been notified, if a promise can run, do it
-    if (this.__activeQueue.length < this.__maxSize) {
-      if (this.__waitingQueue.length > 0) {
-        // Shift the first item out of the waiting queue
-        const firstWaiting = this.__waitingQueue.shift();
-        this.__firePromise(firstWaiting);
-      }
-    }
-  }
+    /**
+     * Fire the promise from the queue object, and pass its resolve or reject back to the caller.
+     * @private
+     *
+     * @param {QueueObject} queueObject
+     */
+    const firePromise = (queueObject) => {
+      // Add it to the active queue and wait for it to finish
+      activeQueue.push(queueObject);
 
-  /**
-   * Fire the promise from the queue object, and pass its resolve or reject back to the caller.
-   * @private
-   *
-   * @param {QueueObject} queueObject
-   */
-  __firePromise (queueObject) {
-    // Add it to the active queue and wait for it to finish
-    this.__activeQueue.push(queueObject);
+      // Once the promise finishes, remove it from the queue and return to the caller
+      const generator = queueObject.getPromiseGenerator();
 
-    // Once the promise finishes, remove it from the queue and return to the caller
-    const generator = queueObject.getPromiseGenerator();
-
-    // This may be a promise, this may not be.
-    // Its a promise according to spec if it has a then method.
-    const potentialPromise = generator();
-    let promise;
-    if (!!potentialPromise.then) {
-      // We have a then method, we're basically a promise
-      promise = potentialPromise;
-    } else {
-      // I don't know what this is, but it's not a promise so resolve it as a Promise.
-      // I hope you're using an ES6 Promise library in 2019.
-      promise = Promise.resolve(potentialPromise);
-    }
-    promise.then(result => {
-      const item = this.__removeItemFromArray(queueObject.getId());
-      if (item != null) {
-        item.resolve(result);
-
-        // Now that the queue has been freed up, notify any waiting Promises
-        this.__notifyWaiting();
-      }
-    }).catch(error => {
-      const item = this.__removeItemFromArray(queueObject.getId());
-      if (item != null) {
-        item.reject(error);
-
-        // Now that the queue has been freed up, notify any waiting Promises
-        this.__notifyWaiting();
-      }
-    });
-  }
-
-  /**
-   * If there is space in the activeQueue, fire the promise immediately, otherwise put it in waiting
-   * @private
-   *
-   * @param {function} promiseGenerator
-   * @return {Promise} A promise to chain off of
-   */
-  __queuePromise (promiseGenerator) {
-    return new Promise((resolve, reject) => {
-      // If there is room for this promise in the active queue
-      const queueObject = new QueueObject(resolve, reject, promiseGenerator);
-      if (this.__activeQueue.length < this.__maxSize) {
-        this.__firePromise(queueObject);
+      // This may be a promise, this may not be.
+      // Its a promise according to spec if it has a then method.
+      const potentialPromise = generator();
+      let promise;
+      if (!!potentialPromise.then) {
+        // We have a then method, we're basically a promise
+        promise = potentialPromise;
       } else {
-        // If there is no room to run, add to the waiting queue
-        this.__waitingQueue.push(queueObject);
+        // I don't know what this is, but it's not a promise so resolve it as a Promise.
+        // I hope you're using an ES6 Promise library in 2019.
+        promise = Promise.resolve(potentialPromise);
       }
-    });
-  };
 
-  /**
-   * Queue up a promise to fire when the Queue is available
-   *
-   * @param {function}promiseGenerator
-   * @return {Promise} A promise to chain off of
-   */
-  queue (promiseGenerator) {
-    if (!promiseGenerator) {
-      throw Error('Must call queue() with a promiseGenerator function');
+      promise.then(result => {
+        const item = removeItemFromArray(queueObject.getId());
+        if (item != null) {
+          item.resolve(result);
+        }
+
+        // Now that the queue has been freed up, notify any waiting Promises
+        notifyWaiting()
+      }).catch(error => {
+        const item = removeItemFromArray(queueObject.getId());
+        if (item != null) {
+          item.reject(error);
+        }
+
+        // Now that the queue has been freed up, notify any waiting Promises
+        notifyWaiting()
+      });
     }
 
-    if (typeof(promiseGenerator) !== TYPE_FUNCTION) {
-      throw Error('promiseGenerator argument must be a function');
+    /**
+     * If there is space in the activeQueue, fire the promise immediately, otherwise put it in waiting
+     * @private
+     *
+     * @param {function} promiseGenerator
+     * @return {Promise} A promise to chain off of
+     */
+    const queuePromise = (promiseGenerator) => {
+      return new Promise((resolve, reject) => {
+        // If there is room for this promise in the active queue
+        const queueObject = new QueueObject(resolve, reject, promiseGenerator);
+        if (activeQueue.length < maxSize) {
+          firePromise(queueObject);
+        } else {
+          // If there is no room to run, add to the waiting queue
+          waitingQueue.push(queueObject);
+        }
+      });
+    };
+
+    /**
+     * Return the current size of the active queue
+     * @return {number}
+     */
+    this.getActiveSize = () => {
+      return activeQueue.length;
     }
 
-    return this.__queuePromise(promiseGenerator);
+    /**
+     * Return the current size of the waiting queue
+     * @return {number}
+     */
+    this.getWaitingSize = () => {
+      return waitingQueue.length;
+    }
+
+    /**
+     * Return the max size of the active queue
+     * @return {number}
+     */
+    this.getMaxSize = () => {
+      return maxSize;
+    }
+
+    /**
+     * Clear the queues on demand
+     */
+    this.clear = () => {
+      // Clear the waiting queue first, any active promises are already active and so will be finished
+      // unless they are cancelled via other means.
+      waitingQueue = [];
+    }
+
+    /**
+     * Queue up a promise to fire when the Queue is available
+     *
+     * @param {function}promiseGenerator
+     * @return {Promise} A promise to chain off of
+     */
+    this.queue = (promiseGenerator) => {
+      if (!promiseGenerator) {
+        throw Error('Must call queue() with a promiseGenerator function');
+      }
+
+      if (typeof(promiseGenerator) !== TYPE_FUNCTION) {
+        throw Error('promiseGenerator argument must be a function');
+      }
+
+      return queuePromise(promiseGenerator);
+    }
   }
-
 }
